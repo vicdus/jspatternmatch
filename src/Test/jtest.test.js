9cfa70a61f2266f8f9fcfcb2351f35b3beab50ed
Moyang <iammoyang@gmail.com>
2018-05-06 01:26:54 -0700

    use jest

---
+++ src/Test/jtest.test.js
@@ -0,0 +1,8 @@
+const Matcher = require('../main/matcher');
+const P = require('../main/pattern').P;
+const Cases = require('../main/case').Cases;
+
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3);
+});
